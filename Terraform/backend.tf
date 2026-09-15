1961d608067aa02b9d10b0c53da4baf2698aea5c
Abhinav Singh <60422234+abhinavsingh-a1@users.noreply.github.com>
2026-09-15 05:46:10 +0200

    Create backend.tf

---
+++ Terraform/backend.tf
@@ -0,0 +1,49 @@
+# For backend, setup Dynamodb table (for maintaining locking state) and amazon S3 bucket (for storing state file).
+
+```terraform
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "tf-s3-bucket-dics"
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-eks-state-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+```
+
+```Bash
+terraform init
+terraform plan
+terraform apply
+```
